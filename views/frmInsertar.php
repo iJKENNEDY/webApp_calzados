276cc59aea07dc9369c03ee0a3fd663a884151f7
enigma Godel <wall.kennedy@hotmail.com>
2020-06-29 16:30:09 -0500

    frmInser...

--- views/frmInsertar.php
+++ views/frmInsertar.php
@@ -3,14 +3,15 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../resources/css/bootstrap.css">
 <title>insercion</title>
 </head>
 <body>
 	<div class="container">
-		<div>
+		<div class="card-body">
 
 			<form method="post" action="../controllers/tiendaController.php">
-				<table>
+				<table class="bg-success">
 					<tr>
 				<td>codigo: </td><input type="text" name="cod" />
 				<td> nombre</td><input type="text" name="nom" /> 
