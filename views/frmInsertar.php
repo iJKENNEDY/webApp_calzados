751de339966314d00c1bcddf1bb8347d51892acf
enigma Godel <qwerty@kmail.com>
2020-06-29 15:51:41 -0500

    agregaando estilos basicos

--- views/frmInsertar.php
+++ views/frmInsertar.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>insercion</title>
 </head>
 <body>
-<form method="post" action="../controllers/tiendaController.php">
-   codigo :<input type="text" name="cod"/>
-   nombre :<input type="text" name="nom"/>
-   marca :<input type="text" name="marc"/>
-   talla:<input type="text" name="tall"/>
-   precio :<input type="text" name="pre"/>
-   <input type="submit" name="btnRegistrar" value="Registrar tienda"/>
-    </form>
+	<div class="container">
+		<div>
+
+			<form method="post" action="../controllers/tiendaController.php">
+				codigo :<input type="text" name="cod" /> nombre :<input type="text"
+					name="nom" /> marca :<input type="text" name="marc" /> talla:<input
+					type="text" name="tall" /> precio :<input type="text" name="pre" />
+				<input type="submit" name="btnRegistrar" value="Registrar tienda" />
+			</form>
+		</div>
+	</div>
 </body>
 </html>
