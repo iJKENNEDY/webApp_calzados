0a65489aba48785b075e51339ca62e8c3969356d
enigma Godel <wall.kennedy@hotmail.com>
2020-06-30 02:18:07 -0500

    se agrega mas campos en el formulario de registro

--- views/frmInsertar.php
+++ views/frmInsertar.php
@@ -13,13 +13,39 @@
 			<form method="post" action="../controllers/tiendaController.php">
 				<table class="bg-success">
 					<tr>
-				<td>codigo: </td><input type="text" name="cod" />
-				<td> nombre</td><input type="text" name="nom" /> 
-				<td> marca </td><input type="text" name="marc" /> 
-				<td> talla</td><input type="text" name="tall" /> 
-				<td> precio</td><input type="text" name="pre" />
-				<input type="submit" name="btnRegistrar" value="Registrar tienda" />
+						<td colspan="2">Registro de calzados</td>
+					</tr>
+					
+				<tr><td>codigo: </td><td><input type="text" name="cod" /></td></tr>
+				<tr><td> nombre</td><td><input type="text" name="nom" /></td> </tr>
+				<tr><td> marca </td><td><input type="text" name="marc" /></td> </tr>
+				<tr><td> talla</td><td><input type="text" name="tall" /></td> </tr>
+				<tr><td> color</td><td>
+						<select name="colores">
+							<option value="rojo">rojo</option>
+							<option value="negro">negro</option>
+							<option value="marron">marron</option>
+						</select>
+					</td>
+				 </tr>
+				 <tr>
+				 	<td>categoria</td>
+				 	<td>
+				 		<input type="radio" name="gcategoria" value="hombre">Hombre
+				 		<input type="radio" name="gcategoria" value="mujer">Mujer
+				 	</td>
+				 </tr>
+				<tr><td> precio</td><td><input type="text" name="pre" /></td></tr>		
+				<tr>
+					<td>
+						cantidad <input type="text" name="cantidad">
+					</td>
 				</tr>
+				<tr>
+					<td colspan="2">
+					<input type="submit" name="btnRegistrar" value="Registrar tienda" />
+					</td>
+				</tr>		
 				</table>
 			</form>
 		</div>
